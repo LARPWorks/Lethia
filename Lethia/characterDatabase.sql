82d1802c2ec6c58e24ebcf31c47eb3d496902caf
Thomas Mc <thomas.mcfarland@gmail.com>
2018-03-29 20:23:11 -0700

    Create characterDatabase.sql
    Initial version imported from Scott Development branch of 2017 Cyaniel project.

---
+++ Lethia/characterDatabase.sql
@@ -0,0 +1,316 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: larpworks
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.22-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `AdvancementListFactGates`
+--
+
+DROP TABLE IF EXISTS `AdvancementListFactGates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `AdvancementListFactGates` (
+  `AdvancementListFactId` int(11) NOT NULL,
+  `RequiredFactId` int(11) NOT NULL,
+  `MinimumRankRequired` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='All gates must be passed for an AdvancementListFact for it to be available for purchase.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `AdvancementListFactModifiers`
+--
+
+DROP TABLE IF EXISTS `AdvancementListFactModifiers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `AdvancementListFactModifiers` (
+  `AdvancementListFactId` int(11) NOT NULL,
+  `FactRequirementId` int(11) NOT NULL,
+  `RequirementRank` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Only one modifier needs to be passed on a advancement list for that fact to be available for selection.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `AdvancementListFacts`
+--
+
+DROP TABLE IF EXISTS `AdvancementListFacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `AdvancementListFacts` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `AdvancementListId` int(11) NOT NULL,
+  `FactId` int(11) NOT NULL,
+  `IsStaffOnly` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=771 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `AdvancementLists`
+--
+
+DROP TABLE IF EXISTS `AdvancementLists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `AdvancementLists` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `IsChargenOnly` bit(1) NOT NULL DEFAULT b'0',
+  `IsStaffOnly` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `AuthenticationTokens`
+--
+
+DROP TABLE IF EXISTS `AuthenticationTokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `AuthenticationTokens` (
+  `Id` binary(16) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `CreatedOn` datetime NOT NULL,
+  UNIQUE KEY `Id_UNIQUE` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `AwardLogs`
+--
+
+DROP TABLE IF EXISTS `AwardLogs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `AwardLogs` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UserId` int(11) NOT NULL,
+  `CharacterId` int(11) DEFAULT NULL,
+  `AwardTypeId` int(11) NOT NULL,
+  `AwardDate` datetime NOT NULL,
+  `Amount` int(11) NOT NULL,
+  `Reason` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `AwardTypes`
+--
+
+DROP TABLE IF EXISTS `AwardTypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `AwardTypes` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `BucketTickets`
+--
+
+DROP TABLE IF EXISTS `BucketTickets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BucketTickets` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `BucketId` int(11) NOT NULL,
+  `Title` varchar(128) NOT NULL,
+  `CreatorId` int(11) NOT NULL,
+  `AssigneeId` int(11) DEFAULT NULL,
+  `Status` int(11) NOT NULL,
+  `CreatedOn` datetime NOT NULL,
+  `LastModified` datetime DEFAULT NULL,
+  `Description` varchar(4096) DEFAULT NULL,
+  `Priority` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Buckets`
+--
+
+DROP TABLE IF EXISTS `Buckets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Buckets` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(64) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `CharacterFacts`
+--
+
+DROP TABLE IF EXISTS `CharacterFacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `CharacterFacts` (
+  `CharacterId` int(11) NOT NULL,
+  `FactId` int(11) NOT NULL,
+  `Rank` int(11) NOT NULL DEFAULT '0',
+  `LastModified` datetime DEFAULT NULL,
+  `Comments` varchar(1024) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Characters`
+--
+
+DROP TABLE IF EXISTS `Characters`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Characters` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(128) NOT NULL,
+  `CreationDate` datetime NOT NULL,
+  `LastUpdate` datetime DEFAULT NULL,
+  `UserId` int(11) NOT NULL,
+  `IsDeleted` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `FactTypes`
+--
+
+DROP TABLE IF EXISTS `FactTypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `FactTypes` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(200) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Facts`
+--
+
+DROP TABLE IF EXISTS `Facts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Facts` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(200) NOT NULL,
+  `Description` varchar(1024) DEFAULT NULL,
+  `FactTypeId` int(11) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=594 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Roles`
+--
+
+DROP TABLE IF EXISTS `Roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Roles` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `TicketAccessLists`
+--
+
+DROP TABLE IF EXISTS `TicketAccessLists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `TicketAccessLists` (
+  `TicketId` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `CanWrite` bit(1) NOT NULL DEFAULT b'0',
+  `CanRead` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `TicketComments`
+--
+
+DROP TABLE IF EXISTS `TicketComments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `TicketComments` (
+  `TicketId` int(11) NOT NULL,
+  `AuthorUserId` int(11) NOT NULL,
+  `Comment` varchar(1024) NOT NULL,
+  `CreatedOn` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `UserRoles`
+--
+
+DROP TABLE IF EXISTS `UserRoles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UserRoles` (
+  `UserId` int(11) NOT NULL,
+  `RoleId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Email` varchar(45) NOT NULL,
+  `Phone` varchar(45) DEFAULT NULL,
+  `Username` varchar(45) NOT NULL,
+  `FirstName` varchar(45) DEFAULT NULL,
+  `LastName` varchar(45) DEFAULT NULL,
+  `BirthDate` varchar(45) DEFAULT NULL,
+  `JoinDate` varchar(45) DEFAULT NULL,
+  `EmergencyContactName` varchar(45) DEFAULT NULL,
+  `EmergencyContactNumber` varchar(45) DEFAULT NULL,
+  `PasswordHash` varchar(512) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Email_UNIQUE` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-07-09  6:51:24
